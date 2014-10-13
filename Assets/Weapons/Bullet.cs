1647ecadb2680b7b1851ba17a4bb06a047bc7dd0
Henry Brown <hankbrown@gmail.com>
2014-10-13 00:51:12 -0500

    Created a new bug with recent changes.

--- Assets/Weapons/Bullet.cs
+++ Assets/Weapons/Bullet.cs
@@ -29,45 +29,16 @@ public class Bullet : MonoBehaviour {
 	void Update () 
   {
   }
-  
-  public void setFacingRight(bool p_facingRight)
-  {
-    this.facingRight = p_facingRight;
-  }
 
   public static GameObject getPrefab()
   {
-    return staticPrefab;
-    //if (prefab == null)
+    if (staticPrefab == null) 
     {
-      /*
-      SpriteRenderer l_spriteRenderer;
-      Rigidbody2D l_rigidBody2D;
-      
-      prefab = new GameObject();
-      
-      l_spriteRenderer = prefab.AddComponent<SpriteRenderer>();
-      
-      l_spriteRenderer.sprite = Sprite.Create((Texture2D) Resources.Load("Weapons/Bullet"), 
-                                              new Rect(Prefabs.DEF_X, Prefabs.DEF_Y, 5f, 25f), 
-                                              Vector2.right);
-
-      l_rigidBody2D = prefab.AddComponent<Rigidbody2D>();
-
-      l_rigidBody2D.mass = 1;
-      l_rigidBody2D.drag = 0;
-      l_rigidBody2D.angularDrag = 0.05f;
-      l_rigidBody2D.gravityScale = 0;
-      l_rigidBody2D.fixedAngle = true;
-      l_rigidBody2D.isKinematic = false;
-
-      prefab.AddComponent<Bullet>();
-      */
-
-      //prefab = 
+      Debug.Log ("New Rifle");
+      new Bullet();
     }
-    
-    //return prefab;
+
+    return staticPrefab;
   }
   
   public void fire()
