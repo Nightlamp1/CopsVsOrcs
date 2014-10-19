b0f7d866cbac2bbd5e47ed1ee39a08e9843c43e2
Henry Brown <hankbrown@gmail.com>
2014-10-19 16:55:53 -0500

    Fixed shooting, deleted .orig files.

--- Assets/Scripts/PowerUpScript.cs
+++ Assets/Scripts/PowerUpScript.cs
@@ -2,14 +2,17 @@
 using System.Collections;
 
 public class PowerUpScript : MonoBehaviour {
-
 	HudScript hud;
+
+  void Start()
+  {
+    hud = GameObject.Find("Main Camera").GetComponent<HudScript>();
+  }
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.tag == "Player")
 		{
-			hud = GameObject.Find("Main Camera").GetComponent<HudScript>();
 			hud.IncreaseScore(10);
 			Destroy (this.gameObject);
 		}
