c7d937b20ca4ab688974de9c57e8616d2f102927
Rob <robert.britt2011@gmail.com>
2015-01-10 16:41:59 -0600

    Yo passthrough like really fucking works now!!!!!!

--- Assets/Scripts/Onewayplat.cs
+++ Assets/Scripts/Onewayplat.cs
@@ -2,11 +2,10 @@
 using System.Collections;
 
 public class Onewayplat : MonoBehaviour {
-
 	
   void Update(){
     GameObject Player = GameVars.getInstance ().getPlayer ();
-    if (Player.transform.position.y < this.transform.position.y) {
+    if (Player.transform.position.y - 1.1 > this.transform.position.y) {
       this.collider2D.isTrigger = false;
     } else {
       this.collider2D.isTrigger = true;
@@ -15,7 +14,7 @@ public class Onewayplat : MonoBehaviour {
 	// Update is called once per frame
 	void OnTriggerEnter2D (Collider2D other) {
 
-    Debug.Log ("Trigger is entered");
+    Debug.Log (System.DateTime.UtcNow.ToString("HH:mm:ss.fff") + "Trigger is entered");
     GameObject Player = other.gameObject;
     if (Player.transform.position.y < this.transform.position.y) {
       Physics2D.IgnoreLayerCollision (0, 2, true);
@@ -27,7 +26,7 @@ public class Onewayplat : MonoBehaviour {
   void OnTriggerExit2D (Collider2D other){
     Debug.Log ("Trigger is exited");
     GameObject Player = other.gameObject;
-    if (Player.transform.position.y > this.transform.position.y) {
+    if (Player.transform.position.y > this.transform.position.y +1.1) {
       Physics2D.IgnoreLayerCollision (0, 2, false);
       this.collider2D.isTrigger = false;
     }
