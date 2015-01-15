c3b931056b5628cd7c4d4dfc3f6a042f4ef47fff
Rob <robert.britt2011@gmail.com>
2015-01-14 20:17:53 -0600

    Added the background art

--- Assets/Scripts/BackgroundScroll.cs
+++ Assets/Scripts/BackgroundScroll.cs
@@ -3,13 +3,12 @@ using System.Collections;
 
 public class BackgroundScroll : MonoBehaviour {
 
-
-	public float speed = 0;
-
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		renderer.material.mainTextureOffset = new Vector2 ((Time.time * speed) % 1, 0f);
-	}
+  public float speed = 0;
+  
+  
+  // Update is called once per frame
+  void Update () {
+    
+    renderer.material.mainTextureOffset = new Vector2 (Time.time * speed, 0f);
+  }
 }
