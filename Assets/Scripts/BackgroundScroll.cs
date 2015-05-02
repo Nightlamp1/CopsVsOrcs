d9a722686e945604506f6c336befd3eca30acfca
Henry Brown <henry@ibespwn.com>
2015-05-01 20:43:37 -0500

    LAB-4: Fixed a NPE that was occuring under certain conditions related to the background scrolling.

--- Assets/Scripts/BackgroundScroll.cs
+++ Assets/Scripts/BackgroundScroll.cs
@@ -8,7 +8,10 @@ public class BackgroundScroll : MonoBehaviour {
   
   // Update is called once per frame
   void Update () {
-    
-    GetComponent<Renderer>().material.mainTextureOffset = new Vector2 (Time.time * speed, 0f);
+    try {
+      GetComponent<Renderer>().material.mainTextureOffset = new Vector2 (Time.time * speed, 0f);
+    } catch (System.Exception ex) {
+      // Ignore it
+    }
   }
 }
