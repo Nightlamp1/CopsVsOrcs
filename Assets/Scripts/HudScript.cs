407d7fab0a2af4615a3e1e934c15999a3eebcda8
Robert Britt <robert.britt2011@gmail.com>
2015-05-08 21:51:12 -0500

    Cleaned up some more of the GUIs:
    1) Main game screen -- applied the offical font and positioned our score trackers correctly
    2) Game over screen -- adjusted button size and positioned things a bit better.
    3) Reduced the frequency of small platform spawns

--- Assets/Scripts/HudScript.cs
+++ Assets/Scripts/HudScript.cs
@@ -2,9 +2,7 @@
 using System.Collections;
 
 public class HudScript : MonoBehaviour {
-  void Start ()
-  {
-  }
+	public Font CVOFont;
 
 	// Update is called once per frame
 	void Update () 
@@ -20,16 +18,18 @@ public class HudScript : MonoBehaviour {
 
 	void OnGUI()
 	{
-    GUI.Label (new Rect (Screen.width * 0.5f, Screen.height * 0.05f, 100, 30), 
+		GUI.color = Color.white;
+		GUI.skin.font = CVOFont;
+    GUI.Label (new Rect (Screen.width * 0.45f, Screen.height * 0.01f, 200, 50), 
                "Score: " + (int)(GameVars.getInstance().score * 10));
 	
-		GUI.Label (new Rect (Screen.width * 0.5f, Screen.height * 0.07f, 100, 30), "Distance: " + (int)(GameVars.getInstance().distance) + "m");
+		GUI.Label (new Rect (Screen.width * 0.01f, Screen.height * 0.01f, 200, 50), "Distance: " + (int)(GameVars.getInstance().distance) + "m");
 
-		GUI.Label (new Rect (Screen.width * 0.5f, Screen.height * 0.09f, 150, 30), "Orcs Destroyed: " + (int)(GameVars.getInstance().orcKills));
+		GUI.Label (new Rect (Screen.width * 0.76f, Screen.height * 0.01f, 200, 50), "Orcs Destroyed: " + (int)(GameVars.getInstance().orcKills));
 	
-    if (GameVars.getInstance().debugMessage != "")
+    /*if (GameVars.getInstance().debugMessage != "")
     {
       GUI.Label (new Rect(Screen.width * 0.5f, Screen.height * 0.13f, 500, 200), GameVars.getInstance().debugMessage);
-    }
+    }*/
 	}
 }
