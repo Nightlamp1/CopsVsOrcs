e1706ff226b269a3d56c7f9d2321ca4f69f4cb38
Henry Brown <henry@ibespwn.com>
2015-06-17 23:32:35 -0500

    LAB-26: Added functionality to GlobalInput.cs to allow people to hit the back button twice to quit -- including a 'toast' of sorts, we could definitely use some better assets here...

--- Assets/Scripts/Input/GlobalInput.cs
+++ Assets/Scripts/Input/GlobalInput.cs
@@ -2,34 +2,54 @@ using UnityEngine;
 using System.Collections;
 
 public class GlobalInput : MonoBehaviour {
-  private const int BACK_BUTTON_DOUBLE_TAP_THRESHOLD = 3; // seconds
+  private const float BACK_BUTTON_DOUBLE_TAP_THRESHOLD = 2.5f; // seconds
 
-  private int timeSinceLastBackButtonPress = 0;
+  private float tSinceBack  = -1f;
+  private float startX      = 0f;
+  private float startY      = 0f;
+  private float width       = 0f;
+  private float height      = 0f;
 
   void Start() {
+    GUI.color = new Color(0, 0, 0, 0);
 
+    width  = Screen.width  * 0.26f;
+    height = Screen.height * 0.05f;
+    startX = (Screen.width - width) / 2f;
+    startY = Screen.height * 0.85f;
   }
 
   void FixedUpdate() {
-    if (timeSinceLastBackButtonPress >= 0) {
-      timeSinceLastBackButtonPress += 1;
+    if (tSinceBack >= 0) {
+      tSinceBack += Time.deltaTime;
     }
 
-    if (timeSinceLastBackButtonPress > BACK_BUTTON_DOUBLE_TAP_THRESHOLD) {
-      timeSinceLastBackButtonPress = -1;
+    if (tSinceBack > BACK_BUTTON_DOUBLE_TAP_THRESHOLD) {
+      tSinceBack = -1;
     }
 
     // This should respond to back button presses and escape keyboard presses.
     //   I don't think we can do anything like this on the iOS devices.
-    if (Input.GetKeyUp(KeyCode.Escape)) {
-      timeSinceLastBackButtonPress = 0;
-
-      if (timeSinceLastBackButtonPress >= 0) {
+    if (Input.GetKeyDown(KeyCode.Escape)) {
+      if (tSinceBack >= 0) {
         Application.Quit();
+
         return;
       } else {
+        tSinceBack = 0f;
+
         return;
       }
+    }
+  }
+
+  void OnGUI() {
+    GUIStyle boxStyle = GUI.skin.GetStyle("Box");
+    boxStyle.fontSize = 30;
+    boxStyle.alignment = TextAnchor.MiddleCenter;
+
+    if (tSinceBack >= 0) {
+      GUI.Box(new Rect(startX, startY, width, height), "Press back again to quit", boxStyle);
     }
   }
 }
