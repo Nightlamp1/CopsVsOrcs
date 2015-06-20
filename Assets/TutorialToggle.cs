b19b28077330f3b68dc44039961476cb30545853
Robert Britt <robert.britt2011@gmail.com>
2015-06-19 23:21:38 -0500

    Different style of tutorial implementation.
    Simply puts the art on the main game screen and disables the renderer for the image if time.timescale =1.
    
    
    Signed-off-by: Robert Britt <robert.britt2011@gmail.com>

---
+++ Assets/TutorialToggle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialToggle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (Time.timeScale == 0)
+			this.gameObject.GetComponent<SpriteRenderer> ().enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.timeScale > 0)
+			this.gameObject.GetComponent<SpriteRenderer> ().enabled = false;
+	}
+}
