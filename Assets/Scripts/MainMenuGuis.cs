a656df496541a0987466843d8f45db3467ff585e
Henry Brown <hankbrown@gmail.com>
2015-06-20 20:24:22 -0500

    Develop: Display development version when based off of develop.

--- Assets/Scripts/MainMenuGuis.cs
+++ Assets/Scripts/MainMenuGuis.cs
@@ -85,7 +85,7 @@ public class MainMenuGuis : MonoBehaviour {
     
 	flexibleSpaces(0);
 
-    GUILayout.Label("v1.0.7");
+    GUILayout.Label("vDEVELOPMENT_VERSION");
 
     GUILayout.EndArea();
 	}
