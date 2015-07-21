18709c50424d86278855bb0272561bba14d8262b
Henry Brown <hankbrown@gmail.com>
2015-07-20 22:32:45 -0500

    LAB-129: Whitespace fix.

--- Assets/Scripts/GUI/GUIManager.cs
+++ Assets/Scripts/GUI/GUIManager.cs
@@ -3,9 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class GUIManager : MonoBehaviour {
+  private BeforeSceneChangeEventHandler leavingDebugBeforeSceneChangeEventHandler;
   public static GUIManager singleton;
 
   private int previousLevel = -1;
+  private Component debugGuiComponent;
 
   [Tooltip("These are the textures for the Debug GUI.  They must match the order of the array below.")]
   public Texture2D[] debugGuiTextures;
@@ -41,6 +43,7 @@ public class GUIManager : MonoBehaviour {
   }
 
   void OnGUI() {
+    // Don't run through this again if we haven't switched levels.
     if (previousLevel == Application.loadedLevel) {
       return;
     }
@@ -60,11 +63,26 @@ public class GUIManager : MonoBehaviour {
         break;
 #if UNITY_EDITOR
       case SceneManager.Scene.DEBUG:
-        gameObject.AddComponent<DebugGUI>();
+        if (Debug.getLogLevel() == Debug.LogLevel.DEBUG && Application.isEditor) {
+          debugGuiComponent = gameObject.AddComponent<DebugGUI>();
+          leavingDebugBeforeSceneChangeEventHandler = new BeforeSceneChangeEventHandler(LeavingDebugScene);
+          SceneManager.getInstance().BeforeSceneChange += leavingDebugBeforeSceneChangeEventHandler;
+        } else {
+          // This error is purposely vague.  I don't want users to understand it.
+          //  Reduces the odds that anyone tries to reverse engineer this to
+          //  figure out what the debug menu is and how to get to it.
+          // This message meaning is "Tried to load the Debug Scene in an invalid configuration"
+          //  because the log level didn't match or because the game isn't running in the editor.
+          Debug.QuietLogError("c3689a90-1b42-4887-a018-858cd039ca76");
+        }
 #endif
         break;
       default:
         break;
     }
+  }
+
+  void LeavingDebugScene(SceneManager.Scene oldScene, SceneManager.Scene newScene) {
+    Destroy(debugGuiComponent);
   }
 }
