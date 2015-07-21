ee965190ec471861055d49b9bc1f5826cec0ebf3
Henry Brown <hankbrown@gmail.com>
2015-07-21 02:35:23 -0500

    LAB-135: Added a long math library to use for statistics calculations.

---
+++ Assets/Scripts/Math/Mathl.cs
@@ -0,0 +1,9 @@
+public class Mathl {
+  public static long max(long l1, long l2) {
+    return (l1 > l2 ? l1 : l2);
+  }
+
+  public static long min(long l1, long l2) {
+    return (l1 < l2 ? l1 : l2);
+  }
+}
