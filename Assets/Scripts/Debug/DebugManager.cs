dfc257e0cca8d572bf6d2f30009e49bfcd2f6630
Henry Brown <hankbrown@gmail.com>
2015-07-20 22:25:17 -0500

    LAB-129: Added a tooltip, fixed a bug in detecting VERSION_NUMBER, and added a QuietLogError event.

--- Assets/Scripts/Debug/DebugManager.cs
+++ Assets/Scripts/Debug/DebugManager.cs
@@ -6,6 +6,7 @@ class DebugManager : MonoBehaviour {
   private static  DebugManager  singleton;
 
 #if UNITY_EDITOR
+  [Tooltip("Set the log level to DEBUG if you want to see the debug scene.  Set it to any other level to only see that severity of log message and up.")]
   public Debug.LogLevel logLevel = Debug.LogLevel.DEBUG;
 #else
   public Debug.LogLevel logLevel = Debug.LogLevel.ERROR;
@@ -23,7 +24,13 @@ class DebugManager : MonoBehaviour {
     DontDestroyOnLoad(gameObject);
 
     // Try to ensure that we don't log anything above ERROR on releases.
-    if (GameVars.VERSION_NUMBER != "vDevelopment") {
+    //  The .ToString() here is a hack so that the C# compiler won't throw
+    //  an error because of unreachable code (it's only unreachable
+    //  if the version number is DEVELOPMENT which it should never be in
+    //  production)
+    if (GameVars.VERSION_NUMBER.ToString() != "DEVELOPMENT" && logLevel != Debug.LogLevel.ERROR) {
+      // ba84c7e0-cfab-4fee-96e7-0c1c4d34c530 = Unexpected logLevel in production.
+      Debug.QuietLogError("ba84c7e0-cfab-4fee-96e7-0c1c4d34c530");
       logLevel = Debug.LogLevel.ERROR;
     }
 
