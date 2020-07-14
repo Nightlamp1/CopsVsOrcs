252069cb9fe371cc2a9b54d426974511b3efe60d
Henry Brown <h@0ti.me>
2020-07-14 09:58:48 -0500

    Better testing for Onewayplat

--- Assets/Scripts/Gameplay/Onewayplat.cs
+++ Assets/Scripts/Gameplay/Onewayplat.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class Onewayplat : MonoBehaviour {
-	
+
   void Update(){
     GameObject Player = GameVars.getInstance().getPlayer();
     if (Player.transform.position.y - 0.8 > this.transform.position.y) {
@@ -11,25 +11,39 @@ public class Onewayplat : MonoBehaviour {
       this.GetComponent<Collider2D>().isTrigger = true;
     }
   }
-	// Update is called once per frame
-	void OnTriggerEnter2D (Collider2D other) {
+  // Update is called once per frame
+  void OnTriggerEnter2D (Collider2D other) {
     GameObject Player = other.gameObject;
 
     if (Player.transform.position.y < this.transform.position.y) {
       Physics2D.IgnoreLayerCollision (0, 2, true);
     }
-	  
-	}
+
+  }
 
 
   void OnTriggerExit2D (Collider2D other){
     GameObject Player = other.gameObject;
+    PlatformerCharacter2D platform = this.GetComponent<PlatformerCharacter2D>();
+    Collider2D collider = this.GetComponent<Collider2D>();
 
-    if (Player.transform.position.y > this.transform.position.y +1.1) {
-      Physics2D.IgnoreLayerCollision (0, 2, false);
-			this.GetComponent<PlatformerCharacter2D>().resetJumpCheck();
-      this.GetComponent<Collider2D>().isTrigger = false;
+    if (Player == null) {
+      Debug.Log("Player was null unexpectedly");
     }
 
+    if (platform == null) {
+      //Debug.Log("Platform was null unexpectedly");
+      return;
+    }
+
+    if (collider == null) {
+      Debug.Log("Collider was null unexpectedly");
+    }
+
+    if (Player.transform.position.y > this.transform.position.y + 1.1) {
+      Physics2D.IgnoreLayerCollision (0, 2, false);
+      platform.resetJumpCheck();
+      collider.isTrigger = false;
+    }
   }
 }
